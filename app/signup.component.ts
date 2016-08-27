e06fa167aabca2f333fad2a2618fcd89c821b066
georgeStanescu <georgestanescu@outlook.com>
2016-08-27 20:19:07 +0300

    switched to the FormBuilder syntax for creating control groups

--- app/signup.component.ts
+++ app/signup.component.ts
@@ -1,15 +1,19 @@
 import {Component,} from "angular2/core"
-import {Control, ControlGroup, Validators} from "angular2/common"
+import {Control, ControlGroup, Validators, FormBuilder} from "angular2/common"
 
 @Component({
   selector: "signup",
   templateUrl: "app/signup.template.html"
 })
 export class SignupComponent {
-  form = new ControlGroup({
-    username: new Control('', Validators.required),
-    password: new Control('', Validators.required)
-  });
+  form: ControlGroup;
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
 
   signup() {
     console.log(this.form.value);
